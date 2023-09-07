204ecfcdad79bfc2333779a9d88409ec2146f7b3
Tallia-Anne <erinawambiekele@gmail.com>
2023-09-07 07:28:57 +0200

    update

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,9 +14,22 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('content', TextareaType::class)
-            ->add('envoyer', SubmitType::class)
+            ->add('email',  EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-contact', 
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-contact', 
+                ],
+            ])
+            ->add('envoyer', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn_contact', 
+                ],
+            ])
         ;
     }
 
